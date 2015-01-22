248a12787d7ca3339d8e98469769c1b4e043df13
vagrant <vagrant@precise32.(none)>
2015-01-22 22:32:01 +0000

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,31 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
-	});
+		$('#testjs').text("Hello World");
+		$(".jumbotron p").addClass("active");
+	});$("a.thumbnail").click(projectClick);}
+	
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+
+
+function projectClick(e) {
+  // Cancel the default action, which prevents the page from reloading
+    e.preventDefault();
+
+    // In an event listener, $(this) is the leement that fired the event
+    var projectTitle = $(this).find("p").text();
+    var jumbotronHeader = $(".jumbotron h1"); //calls on class jumbotron # calls on specific
+    jumbotronHeader.text(projectTitle);
+         var containingProject = $(this).closest(".project"); 
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) { 
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>"); 
+    } else { 
+       $(description).fadeOut();
+    }
 }
+
+
+
